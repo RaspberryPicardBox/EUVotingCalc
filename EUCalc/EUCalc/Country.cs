0fcdd570802f8370bb9fbe873288daa4c53e370b
Michael Boyce <raspberrypicardbox@gmail.com>
2020-02-19 13:19:40 +0000

    Began C# proj; added a list of countries and country class.

---
+++ EUCalc/EUCalc/Country.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EUCalc
+{
+    class Country
+    {
+        private string _name;
+        private int _population;
+        private bool _abstain;
+        private bool _vote;
+
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public int population
+        {
+            get { return _population; }
+            set { _population = value; }
+        }
+
+        public Country(string newname, int newpopulation)
+        {
+            name = newname;
+            population = newpopulation;
+        }
+    }
+}
---
+++ EUCalc/EUCalc/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EUCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Country> Countries = new List<Country> { new Country("Germany", 22), new Country("France", 50) } ;
+
+            foreach (Country x in Countries)
+            {
+                Console.WriteLine($"{x.name} & {x.population}");
+            }
+
+        }
+    }
+}
