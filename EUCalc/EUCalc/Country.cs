17c717216d4d91903df54d96619cc370fa114d33
Michael Boyce <raspberrypicardbox@gmail.com>
2020-02-19 14:44:38 +0000

    Added a text file for countries. Read from to input to list.

--- EUCalc/EUCalc/Country.cs
+++ EUCalc/EUCalc/Country.cs
@@ -7,7 +7,7 @@ namespace EUCalc
     class Country
     {
         private string _name;
-        private int _population;
+        private double _population;
         private bool _abstain;
         private bool _vote;
 
@@ -16,13 +16,22 @@ namespace EUCalc
             get { return _name; }
             set { _name = value; }
         }
-        public int population
+        public double population
         {
             get { return _population; }
             set { _population = value; }
         }
-
-        public Country(string newname, int newpopulation)
+        public bool abstain
+        {
+            get { return _abstain; }
+            set { _abstain = value; }
+        }
+        public bool vote
+        {
+            get { return _vote; }
+            set { _vote = value; }
+        }
+        public Country(string newname, double newpopulation)
         {
             name = newname;
             population = newpopulation;
--- EUCalc/EUCalc/Program.cs
+++ EUCalc/EUCalc/Program.cs
@@ -7,13 +7,68 @@ namespace EUCalc
     {
         static void Main(string[] args)
         {
-            List<Country> Countries = new List<Country> { new Country("Germany", 22), new Country("France", 50) } ;
+            int Yes = 0;
+            int No = 0;
+            int Abstain = 0;
 
-            foreach (Country x in Countries)
+            List<Country> Countries = new List<Country>();
+
+            string[] content = System.IO.File.ReadAllLines(@"..\..\..\countryList.txt");
+            foreach (string line in content)
             {
-                Console.WriteLine($"{x.name} & {x.population}");
+                string[] words = line.Split(",");
+                Countries.Add(new Country(words[0], Double.Parse(words[1])));
             }
 
+            int total = Countries.Count;
+
+            foreach (Country country in Countries)
+            {
+                Console.WriteLine($"For {country.name}, would you like to abstain? (Y/N)");
+                string input = Console.ReadLine().ToUpper();
+                if (input == "Y")
+                {
+                    country.abstain = true;
+                    Abstain += 1;
+                }
+                else if(input == "N")
+                {
+                    country.abstain = false;
+                }
+                else
+                {
+                    Console.WriteLine($"{input} is not a valid answer. Defaulting to N...");
+                    country.abstain = false;
+                }
+
+                if (country.abstain == false)
+                {
+                    Console.WriteLine($"Would you like to vote for {country.name}? (Y/N)");
+                    input = Console.ReadLine().ToUpper();
+                    if (input == "Y")
+                    {
+                        country.vote = true;
+                        Yes += 1;
+                    }
+                    else if (input == "N")
+                    {
+                        country.vote = false;
+                        No += 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{input} is not a valid answer. Defaulting to Y...");
+                        country.vote = true;
+                        Yes += 1;
+                    }
+                }
+
+            }
+
+            int percentYes = (total / 100) * Yes;
+            int percentNo = (total / 100) * No;
+            int percentAbstain = (total / 100) * Abstain;
+            Console.WriteLine($"Out of total {total}, {Yes} were voted for, giving {percentYes}%. {No} were voted against, giving {percentNo}%, and {Abstain} abstained giving {percentAbstain}%.");
         }
     }
 }
